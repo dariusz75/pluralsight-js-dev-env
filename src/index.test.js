31715c1bdc063121918a3573de4d91394ad97d9b
Dariusz Franczak <darius32@op.pl>
2017-04-24 22:44:22 +0100

    testing setup

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('Should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
+
